0c215aa8b2984dfa32f2b61a32e8f1462e6bf7db
ABDUL-REHMAN <abdulrehman034551@gmail.com>
2024-05-23 17:37:29 +0500

    Login Functionality Perform

--- app.js
+++ app.js
@@ -15,6 +15,13 @@ let title = document.getElementById("title");
 let download_music = document.getElementById("download_music");
 let shuffle = document.getElementsByClassName("shuffle")[0];
 
+// Filter Song Functionality Add
+let input = document.getElementsByTagName("input")[0];
+
+// Search Data Start
+// Target Search bar Functionality
+let search_results = document.getElementsByClassName("search_results")[0];
+
 // Add Next & Back Button Functionality
 let back = document.getElementById("back");
 let next = document.getElementById("next");
@@ -704,4 +711,52 @@ music.addEventListener("ended", () => {
       random_music();
       break;
   }
+});
+
+// Search Data Start
+songs.forEach((element) => {
+  const { id, songName, poster } = element;
+
+  // console.log(id);
+  // console.log(songName);
+  // console.log(poster);
+
+  let card = document.createElement("a");
+  card.classList.add("card");
+  card.href = "#" + id;
+
+  card.innerHTML = `<img src="${poster}" alt="" />
+  <div class="content">
+    ${songName}
+  </div>`;
+  search_results.appendChild(card);
+});
+
+// Search Song Filter & Items Songs Functionality
+input.addEventListener("keyup", () => {
+  let input_value = input.value.toLocaleUpperCase();
+  let items = search_results.getElementsByTagName("a");
+
+  for (let index = 0; index < items.length; index++) {
+    let as = items[index].getElementsByClassName("content")[0];
+    let text_value = as.textContent || as.innerHTML;
+
+    if (text_value.toLocaleUpperCase().indexOf(input_value) > -1) {
+      items[index].style.display = "flex";
+    } else {
+      items[index].style.display = "none";
+    }
+
+    //  jb kch na likho search bar ma tho kch show na ho function
+
+    if (input.value == 0) {
+      search_results.style.display = "none";
+    } else {
+      search_results.style.display = "";
+      
+    }
+
+
+  }
+ 
 });
